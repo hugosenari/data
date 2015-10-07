4b345004876ef6d15e5f6fd98cf4a56c8202483b
hugosenari <hugosenari@gmail.com>
2015-10-07 10:56:36 -0200

    add basic tpl

--- flaskapp.py
+++ flaskapp.py
@@ -4,14 +4,37 @@ Created on Oct 7, 2015
 @author: hugosenari
 '''
 
+from flask import Flask, render_template, Response, request
+import requests
 
-from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/curl/')
+def curl():
+    url = request.args.get('url')
+    if not url:
+        return Response('{}', status=200, mimetype='application/json')
+
+    env_key = request.args.get('key', '')
+    key = os.env.get(env_key, '')
+
+    url = url.format(key=key)
+    my_request = requests.get(url)
+    my_request.raise_for_status()
+    json = my_request.text
+
+    return Response(json, status=200, mimetype='application/json')
+
+
+@app.route('/<post>')
+def post(path):
+    return render_template('{}/index.html'.format(post))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
